5df3a7d7dc5db3a5c0c964f18658ac598a5ad76e
DidrikGetacademy <141577441+DidrikGetacademy@users.noreply.github.com>
2023-11-23 16:20:57 +0100

    Add files via upload
    Parprogrammering

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+
+namespace ParProgrammeringPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<VirtualPet> petNames = new List<VirtualPet>();
+            Console.WriteLine("what will you call your pet?");
+            string petName = Console.ReadLine();
+
+            var pet = new VirtualPet(petName);
+
+            int choice;
+            do
+            {
+
+                Console.WriteLine($"1.feed {pet.name} ");
+                Console.WriteLine($"2.cuddle {pet.name}");
+                Console.WriteLine($"3.check if {pet.name} needs to go to the toilet");
+                Console.WriteLine("4.add a new pet");
+                Console.WriteLine("5.show all pets");
+                Console.WriteLine("Velg en handling(1-5)");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+                switch (choice)
+                {
+                    case 1:
+                        pet.feed();
+                        break;
+                    case 2:
+                        pet.cuddle();
+                        break;
+                    case 3:
+                        pet.toilet();
+                        break;
+                    case 4:
+                        Console.WriteLine("What will you call your new pet?");
+                        string newPetNames = Console.ReadLine();
+                        var newPet = new VirtualPet(newPetNames);
+                        petNames.Add(newPet);
+                        Console.WriteLine($"{newPetNames} has been added!");
+                        newPet.visStatus();
+                        break;
+                    case 5:
+                       pet.ShowAllPets(petNames);
+
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please try again!");
+                        break;
+                }
+               
+                Console.WriteLine();
+               
+                pet.visStatus();
+            } while (choice != 0);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+/* Console.WriteLine("3.check if animal needs to go to the bathroom");
+               Console.WriteLine("4.Thank you for giving me food");
+               Console.WriteLine("5.Thank you for the cuddles");
+               Console.WriteLine("6.Thank you for giving me a toilet break");
+              */
+
+
+
+
+
+/*Lag en konsoll-applikasjon hvor man kan generere opp flere forskjellige virtuelle kjæledyr.
+Bruk constructor i klassen, og man skal ikke ha lov til å endre data som navn, age eller noen andre egenskaper utenfra klassen. Lag funksjonalitet som gjør at man kan gi dyret mat, kose med dyret eller sjekke om dyret må på do
+Her er et eksempel på hvordan det kan se ut:
+Hvilket dyr vil du ta vare på?
+Pikachu
+1. Gi Pikachu mat
+2. Kos med Pikachu
+3. Sjekk om Pikachu må på do
+2
+Pikachu smiler!
+1. Gi Pikachu mat
+2. Kos med Pikachu
+3. Sjekk om Pikachu må på do
+1
+Pikachu er mett og fornøyd*/
+
+
+
+// list<pet> pets = new list <pet>(PetName)for å ha en liste av alle pets
+//pets.add(petname)
+// var newpet = new virtualpet(petname) ??
+
+// for each (var pet in pets){}
---
+++ VirutalPet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProgrammeringPar
+{
+    class VirtualPet
+    {
+        public string name;
+        public int age;
+        public bool hungry;
+        public bool nuggle;
+        public bool WC;
+
+
+        public VirtualPet(
+            string name,
+            int age = 1,
+            bool hungry = true,
+            bool nuggle = true,
+            bool WC = false
+
+        )
+        {
+            this.name = name;
+            this.age = age;
+            this.hungry = hungry;
+            this.nuggle = nuggle;
+            this.WC = WC;
+
+        }
+
+        public void visStatus()
+        {
+            Console.WriteLine($"{name} - {age} years old, " +
+                              $"{(hungry ? "Hungry," : "Full,")} " +
+                              $"{(nuggle ? "Want cuddles," : "Cuddled,")}" +
+                              $"{(WC ? "needs toilet" : "i dont need toilet;")}"
+            );
+        }
+
+
+        public void feed()
+        {
+            if (hungry)
+            {
+                Console.WriteLine("Thank you im now full and satisfied");
+                hungry = false;
+            }
+        }
+
+
+
+        public void cuddle()
+        {
+            if (nuggle)
+            {
+                Console.WriteLine("Thank you for the cuddle!");
+                nuggle = false;
+            }
+        }
+
+
+
+        public void toilet()
+        {
+            if (WC)
+            {
+                Console.WriteLine("thank you, i really needed that");
+            }
+            else Console.WriteLine("i don't need to go to the toilet right now");
+        }
+
+        public void ShowAllPets(List<VirtualPet> virtualPets)
+        {
+            Console.WriteLine("All Pets:");
+
+            foreach (var pet in virtualPets)
+            {
+                Console.WriteLine($"{pet.name} - {pet.age} years old" +
+                                  $"{(pet.hungry ? "Hungry," : "Full,")} " +
+                                  $"{(pet.nuggle ? "Want cuddles," : "Cuddled,")}" +
+                                  $"{(pet.WC ? "needs toilet" : "i dont need toilet;")}");
+                
+            }
+
+        
+        }
+    }
+}
+
